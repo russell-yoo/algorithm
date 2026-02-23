aa7d95a9df621c24e006f0b4b39a9709fc78535e
russell yoo <russelly@kakao.com>
2026-02-23 22:46:16 +0900

    feat: LeetCode 1461 풀이 완성

--- ps/leetcode/java/1461/Solution.java
+++ ps/leetcode/java/1461/Solution.java
@@ -1,4 +1,13 @@
+import java.util.*;
+
 class Solution {
 
-    public boolean hasAllCodes(String s, int k) {}
+    public boolean hasAllCodes(String s, int k) {
+        if (s.length() < k) return false;
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i <= s.length() - k; i++) {
+            set.add(s.substring(i, i + k));
+        }
+        return set.size() == 1 << k;
+    }
 }
