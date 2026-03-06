2060223189882ad0b646f0f32b7af754fca50b0f
russell yoo <russelly@kakao.com>
2026-03-06 22:29:18 +0900

    feat: LeetCode 1784 풀이 추가

---
+++ ps/leetcode/java/1784/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public boolean checkOnesSegment(String s) {
+        int n = s.length();
+        if (n == 1) {
+            return true;
+        }
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i - 1) == '0' && s.charAt(i) == '1') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
